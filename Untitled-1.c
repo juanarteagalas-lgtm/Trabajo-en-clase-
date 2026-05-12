75020d92d5495c0c10cb002a4f6449bd14450cbf
Juan Arteaga <juan.arteaga.lsaluisa@udla.edu.ec>
2026-05-12 11:20:50 -0500

    segunda opcion de impresion del arreglo con  punteros

--- Untitled-1.c
+++ Untitled-1.c
@@ -8,8 +8,9 @@ int main() {
     ptr = &arreglo[0];
 
     for (int i = 0; i < 5; i++) {   //imprimir los elementos del arreglo 
-        printf("elemento %d: %d \n", i, arreglo[i]);   
-        printf("%d", ptr);
+        printf("elemento %d: %d \n", i, arreglo[i]); 
+        //  ptr = &arreglo[i];
+        printf("%d \n", *(ptr + i));
     }
 
     return 0;
